4b9419f7f1d0864a98a808c22f280b068ac054ef
kiran-20021912 <kirandevang055@gmail.com>
2024-10-28 14:49:29 +0530

    commited

--- feature.py
+++ feature.py
@@ -1,1 +1,13 @@
 abc
+S C:\Users\Amrutha\Desktop\kiranassignment\IBMAssign> git log
+commit 6521bd9a07807ab1d3e39081afa3244aa08645e3 (HEAD -> new-feature, origin/new-feature)
+Author: kiran-20021912 <kirandevang055@gmail.com>
+Date:   Mon Oct 28 14:44:53 2024 +0530
+
+    new file
+
+commit 0ddcc4635ab48c0f7b975da81523bb44fde3e4b6 (origin/main, origin/HEAD, main)
+Author: kiran-20021912 <kirandevang055@gmail.com>
+Date:   Mon Oct 28 14:30:03 2024 +0530
+
+    Initial commit
